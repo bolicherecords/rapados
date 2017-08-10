4c517632d521f65dc7fb9ff74b9d12921e69bc19
tucocorp <tucocorp@gmail.com>
2017-08-10 00:03:14 -0400

    show in angular

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,10 +1,14 @@
 angular.module('RapadosApp', ['ui.router', 'rapadosControllers','ngMaterial'])
   .config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider) { // provider-injector
-    $stateProvider.state('example', {
-         url: "/example",
-         templateUrl: "/example"
+    $stateProvider.state('provider', {
+         url: "/providers/{provider:json}",
+         templateUrl: "/providers/show.haml",
+         controller: function($scope, $stateParams) {
+           return provider = $stateParams;
+         }
      });
   }])
-  function AppCtrl($scope) {
-    $scope.currentNavItem = 'page1';
-  }
+
+  // function AppCtrl($scope) {
+  //   $scope.currentNavItem = 'page1';
+  // }
--- app/assets/javascripts/angular/controllers/providerController.js
+++ app/assets/javascripts/angular/controllers/providerController.js
@@ -1,6 +1,7 @@
 controllers.controller('ProviderController', ['$scope', '$http', '$state', '$window', function($scope, $http, $state, $window){
-	
+
 	$scope.providers = [];
+	$scope.provider = $state.params.provider;
 
 	$scope.getProviders = function(providers){
 		for (var i=0; i<providers.length; i++){
@@ -8,4 +9,8 @@ controllers.controller('ProviderController', ['$scope', '$http', '$state', '$win
 		}
 	};
 
+	$scope.setProvider = function(provider){
+		debugger
+		$scope.provider = provider ? provider : null;
+	};
 }]);
