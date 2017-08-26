3bc33b34fdf1b9371de4ea3593b409fd898c505c
tucocorp <tucocorp@gmail.com>
2017-08-26 11:16:44 -0300

    Navbar, remove material design until best configuration

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
   resources :clients
   resources :purchases
   resources :sales
+
+  # devise_for :users, :path_prefix => 'my'
+  devise_scope :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
 end
