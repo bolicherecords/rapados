1a5aaa50773c30d0a7d8f1d1f0dc0a5ac9ba6e1c
tucocorp <tucocorp@gmail.com>
2017-09-05 00:23:43 -0300

    remove client relation from Sale

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -9,12 +9,12 @@ class Sale
   STATUS_DRAFT            = 1
   STATUS_FINISHED         = 2
   STATUS_CANCELLED        = 3
-  
+
 
   # == Asociaciones
   has_many   :sale_details
   belongs_to :store
-  belongs_to :client
+  # belongs_to :client
   belongs_to :user
 
   # == Atributos
