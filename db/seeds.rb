ef14d858736ca391925eef0cf6d51df475d852db
tucocorp <tucocorp@gmail.com>
2017-10-09 22:41:21 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-@user = User.find_or_create_by(email: "gonzalo@tetracode.cl", password: "12345678")
+@store1 = Store.find_or_create_by(name: 'Central Santiago', city: 'Santiago', country: 'Chile', timezone: '-3', status: 1)
+@store2 = Store.find_or_create_by(name: 'Central Isla', city: 'Isla Pascua', country: 'Chile', timezone: '-5', status: 1)
+
+@user = User.find_or_create_by(email: 'gonzalo@tetracode.cl', password: "12345678")
 @user.save
 @user.add_role :god
 
-@user = User.find_or_create_by(email: "francisco@tetracode.cl", password: "12345678")
+@user = User.find_or_create_by(email: 'francisco@tetracode.cl', password: '12345678')
 @user.save
 @user.add_role :god
 
-@user = User.find_or_create_by(email: "admin@tetracode.cl", password: "12345678")
-@user.save
-@user.add_role :admin
+@user1 = User.find_or_create_by(email: 'admin@tetracode.cl', password: '12345678', store_id: @store1.id)
+@user1.save
+@user1.add_role :admin
 
-@user = User.find_or_create_by(email: "seller@tetracode.cl", password: "12345678")
+@user = User.find_or_create_by(email: 'seller@tetracode.cl', password: '12345678', store_id: @store2.id)
 @user.save
 @user.add_role :seller
 
