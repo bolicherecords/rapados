027871968c6407ce7104d6012dac855896e73a23
rapbore <francisco.abalan@gmail.com>
2017-12-16 14:11:23 -0300

    dev expenses and contributions 2

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -18,27 +18,27 @@ class ContributionsController < ApplicationController
   def create
     store_id = contribution_params[:store_id].present? ? contribution_params[:store_id] : current_user.store_id
     contribution = Contribution.create(user: current_user, store_id: store_id, name: contribution_params[:name], price: contribution_params[:price])
-    flash[:success] = 'Egreso creado exitosamente.'
+    flash[:success] = 'Aporte creado exitosamente.'
     redirect_to contributions_url
   end
 
   def edit
     case params[:origin]
     when "CANCELLED"
-      @contribution.cancel ? flash[:success] = 'Egreso anulado exitosamente.' : flash[:danger] = "Imposible anular egreso. Egreso #{ContributionDecorator.decorate(@contribution).status_name}."
+      @contribution.cancel ? flash[:success] = 'Aporte anulado exitosamente.' : flash[:danger] = "Imposible anular egreso. Aporte #{ContributionDecorator.decorate(@contribution).status_name}."
       redirect_to contributions_url
     end
   end
 
   def update
     @contribution.update(contribution_params)
-    flash[:success] = 'Egreso actualizado exitosamente.'
+    flash[:success] = 'Aporte actualizado exitosamente.'
     redirect_to contributions_url
   end
 
   def destroy
     @contribution.destroy
-    redirect_to contributions_url, notice: 'Egreso eliminado exitosamente.'
+    redirect_to contributions_url, notice: 'Aporte eliminado exitosamente.'
   end
 
   private
