3c46a34319ab51baa374a6e3648e959146cb37d5
rapbore <francisco.abalan@gmail.com>
2018-01-17 23:30:55 -0300

    dev cashflow without store 2

--- app/services/fetchers/fetch_cash_flows_service.rb
+++ app/services/fetchers/fetch_cash_flows_service.rb
@@ -2,7 +2,7 @@ class Fetchers::FetchCashFlowsService < BaseService
   def self.execute(params = {})
     query = params[:query]
     status = params[:status]
-    cash_flows = status ? CashFlow.where(status: status) : CashFlow.where(:status.ne => CashFlow::STATUS_CANCELLED)
+    cash_flows = status ? CashFlow.where(status: status) : CashFlow.where(:status.ne => CashFlow::STATUS_FINISHED)
     cash_flows = CashFlow.all
     cash_flows = cash_flows.full_text_search(query) if (query.present? && query != " ")
     cash_flows = cash_flows.order(created_at: :desc)
